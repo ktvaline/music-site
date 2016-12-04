eb20344d0f1d5de2ffaeee6b3eb7a139de4e0641
Lukas Valine <lukas@valine.io>
2016-12-04 17:07:40 -0600

    message working

--- app/server.js
+++ app/server.js
@@ -24,21 +24,20 @@ app.post('/sendmail', function(request, response, body){
 
    console.log(JSON.stringify(request.body));
    
+    var password = "passhere"
 
-
-/*
 	var nodemailer = require('nodemailer');
 
 	// create reusable transporter object using the default SMTP transport
-	var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+	var transporter = nodemailer.createTransport('smtps://benvalinesite%40gmail.com:' + password + '@smtp.gmail.com');
 
 	// setup e-mail data with unicode symbols
 	var mailOptions = {
-		from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
-		to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-		subject: 'Hello ✔', // Subject line
-		text: 'Hello world ?', // plaintext body
-		html: '<b>Hello world ?</b>' // html body
+		from: '"' + request.body.value.name + ' ?" <' + request.body.value.email + '>', // sender address
+		to: 'benvalinesite@gmail.com', // list of receivers
+		subject: 'from name: ' + request.body.value.name + ' email: ' + request.body.value.email, // Subject line
+		text: request.body.value.message, // plaintext body
+		html: request.body.value.message // html body
 	};
 
 	// send mail with defined transport object
@@ -48,8 +47,6 @@ app.post('/sendmail', function(request, response, body){
 		}
 		console.log('Message sent: ' + info.response);
 	});
-	* 
-*/
 
     
     response.send("success");
